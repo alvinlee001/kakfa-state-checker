2a918e4b7ab0d9c2bcf9488955537824de8f71bd
Alvin Lee Jin Wen <alvinlee001@live.com.my>
2022-07-14 23:12:13 +0800

    spring initializer initial commit

---
+++ src/main/java/com/kafkastatechecker/KafkaStateCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.kafkastatechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStateCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStateCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkastatechecker/KafkaStateCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkastatechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStateCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
